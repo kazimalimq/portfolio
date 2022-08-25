3e806d657854cc708863a405634151a6d61c240b
kazimalimq <ali.kazim@students.mq.edu.au>
2022-08-26 08:52:21 +1000

    Header with sample image and Resume

---
+++ src/components/header/HeaderSocials.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import {BsLinkedin} from 'react-icons/bs'
+import {FaGithub} from 'react-icons/fa'
+
+const HeaderSocials = () => {
+  return (
+    <div className='header__socials'>
+        <a href='https://github.com/kazimalimq' target="_blank"><BsLinkedin/></a>
+        <a href='https://github.com/kazimalimq' target="_blank"><FaGithub/></a>
+        <a href='https://github.com/kazimalimq' target="_blank"></a>
+        
+        </div>
+  )
+}
+
+export default HeaderSocials
